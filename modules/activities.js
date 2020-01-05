654c56a4e023ab93d4ca1245afbbd5ce725fbfe2
Mohsen <33019552+mohsenoffice@users.noreply.github.com>
2020-01-06 00:17:43 +0200

    add duration of watched videos (#17)

--- modules/activities.js
+++ modules/activities.js
@@ -4,7 +4,8 @@ const {Schema} = mongoose;
 const activitiesSchema = new Schema({
     user: {type: String, unique: true, index: true, required: true},
     searches: [String],
-    watched: Array
+    watched: Array,
+    duration:{trype: Number, default:0}
 })
 
 mongoose.model('activities', activitiesSchema);  
